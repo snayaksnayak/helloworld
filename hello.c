0be3f5010d4362be7c53b0d17406a1ab7c71730d
snayaksnayak <sinu.nayak2001@gmail.com>
2012-11-03 15:47:24 +0530

    first commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/* Program to diplay hello world */
+/* Date: 03 Nov 2012 */
+/* Author: Srinivas Nayak */
+/* Compile using gcc */
+
+/* This code is distributed under the GPL License. */
+/* For more info check: */
+/* http://www.gnu.org/copyleft/gpl.html */
+
+#include<stdio.h>
+int main()
+{ 
+	printf("hello world.\n");
+	return 0;
+}
